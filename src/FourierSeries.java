383e9e0d943229bfcf272a7c953b275e4e0a35a9
kdzumba <knowledgedzumba@gmail.com>
2022-06-28 01:56:01 +0200

    sawtooth wave and const cleanup

--- src/FourierSeries.java
+++ src/FourierSeries.java
@@ -2,82 +2,45 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FourierSeries extends JFrame implements ActionListener
 {
-    private final int LEFT = 100;
-    private final int UP = 200;
-    private final int WINDOW_WIDTH = 1900;
-    private final int WINDOW_HEIGHT = 1200;
-    private final int CURSOR_X_TRANSLATION = 400;
-    private final int TIMER_DELAY = 10;
-    private final int AMPLITUDE_SCALER = 400;
     private float angle = 0f;
-    private final double HARMONIC_FREQUENCY = 0.05;
-    private final List<HarmonicVector> harmonics;
     private final List<Double> waveHeights;
-    private Image doubleBufferImage;
-    private Graphics doubleBufferGraphics;
+    private final PeriodicWaves waves = new PeriodicWaves();
     public FourierSeries(String title)
     {
         this.waveHeights = new ArrayList<>();
-        this.harmonics = new ArrayList<>();
-
-        addHarmonics(1);
-
-        Timer timer = new Timer(TIMER_DELAY, this);
+        Timer timer = new Timer(Constants.TIMER_DELAY, this);
         timer.start();
 
-        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setSize(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
         setTitle(title);
         setResizable(false);
         getContentPane().setBackground(Color.BLACK);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-
-    public void addHarmonics(int harmonicCount)
-    {
-        for(int n = 0; n < harmonicCount; n++)
-        {
-            var coefficient = 2 * n + 1;
-            var amplitude = AMPLITUDE_SCALER / (coefficient * Math.PI);
-            HarmonicVector harmonic;
-
-            if(n == 0)
-            {
-                harmonic = new HarmonicVector(new Pair(LEFT, UP), new Pair(LEFT + amplitude * 2, UP + amplitude), amplitude, coefficient);
-            }
-            else
-            {
-                harmonic = new HarmonicVector(new Pair(harmonics.get(n - 1).getTerminalPoint().x - amplitude, harmonics.get(n - 1).getTerminalPoint().y - amplitude),
-                        new Pair(harmonics.get(n - 1).getTerminalPoint().x + amplitude, harmonics.get(n - 1).getTerminalPoint().y),
-                        amplitude, coefficient);
-            }
-
-            if(n == harmonicCount - 1)
-            {
-                harmonic.setIsLastHarmonic(true);
-            }
-            this.harmonics.add(harmonic);
-        }
-    }
-
     public void drawCursor(Graphics2D g, Pair initialPoint, Pair terminalPoint)
     {
         g.draw(new Line2D.Double(initialPoint.x, initialPoint.y, terminalPoint.x, terminalPoint.y));
     }
 
+    public void drawAxis(Graphics2D g, Pair origin)
+    {
+        g.setColor(Color.white);
+        g.draw(new Line2D.Double(0, origin.y, Constants.WINDOW_WIDTH, origin.y));
+        g.draw(new Line2D.Double(origin.x, Constants.WINDOW_HEIGHT, origin.x, 0));
+    }
     private void removeOffScreenPoints()
     {
         //Use traditional for loop here to avoid ConcurrentModificatoinException
         for(int index = 0; index < waveHeights.size(); index++)
         {
-            if(index + CURSOR_X_TRANSLATION > WINDOW_WIDTH)
+            if(index + Constants.CURSOR_X_TRANSLATION > Constants.WINDOW_WIDTH)
             {
                 waveHeights.remove(waveHeights.get(index));
             }
@@ -86,12 +49,17 @@ public class FourierSeries extends JFrame implements ActionListener
     @Override
     public void paint(Graphics g)
     {
-        doubleBufferImage = createImage(getWidth(), getHeight());
-        doubleBufferGraphics = doubleBufferImage.getGraphics();
+        Image doubleBufferImage = createImage(getWidth(), getHeight());
+        Graphics doubleBufferGraphics = doubleBufferImage.getGraphics();
 
         super.paint(doubleBufferGraphics);
         Graphics2D g2d = (Graphics2D) doubleBufferGraphics;
+
+        //TODO: co-ordinates for the origin here are reaaaalllly random numbers that just happen to fit, need to do this right
+        drawAxis(g2d, new Pair(245, 295));
         doubleBufferGraphics.setColor(Color.WHITE);
+
+        var harmonics = waves.getSawToothWave(100);
 
         for(int n = 0; n < harmonics.size(); n++)
         {
@@ -107,28 +75,25 @@ public class FourierSeries extends JFrame implements ActionListener
                 harmonic.setTerminalPoint(new Pair(harmonic.getInitialPoint().x + harmonic.getRadius() + harmonic.getRadius() * Math.cos(harmonic.getFrequency() * angle),
                         harmonic.getInitialPoint().y + harmonic.getRadius() + harmonic.getRadius() * Math.sin(harmonic.getFrequency() * angle)));
             }
-
             harmonic.paintComponent(g2d);
 
             if(harmonic.getIsLastHarmonic())
             {
-                drawCursor(g2d, harmonic.getTerminalPoint(), new Pair(CURSOR_X_TRANSLATION, harmonic.getTerminalPoint().y));
+                drawCursor(g2d, harmonic.getTerminalPoint(), new Pair(Constants.CURSOR_X_TRANSLATION, harmonic.getTerminalPoint().y));
                 this.waveHeights.add(0, harmonic.getTerminalPoint().y);
             }
         }
 
         this.removeOffScreenPoints();
-        waveHeights.forEach(height -> g2d.draw(new Line2D.Double(CURSOR_X_TRANSLATION + waveHeights.indexOf(height), height, CURSOR_X_TRANSLATION + waveHeights.indexOf(height), height)));
+        waveHeights.forEach(height -> g2d.draw(new Line2D.Double(Constants.CURSOR_X_TRANSLATION + waveHeights.indexOf(height), height, Constants.CURSOR_X_TRANSLATION + waveHeights.indexOf(height), height)));
         g.drawImage(doubleBufferImage, 0, 0, this);
     }
-
     public static void main(String[] args) {
         new FourierSeries("Fourier Series");
     }
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        angle += HARMONIC_FREQUENCY;
+        angle += Constants.HARMONIC_FREQUENCY;
         this.repaint();
     }
 }
