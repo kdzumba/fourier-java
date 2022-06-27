25facd23474f73bfcaf794b2443f8afff4fb6929
kdzumba <knowledgedzumba@gmail.com>
2022-06-28 01:57:30 +0200

    forgot to add files

---
+++ src/PeriodicWaves.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PeriodicWaves
+{
+    private final List<HarmonicVector> harmonics;
+    public PeriodicWaves()
+    {
+        harmonics = new ArrayList<>();
+    }
+    public List<HarmonicVector> getSquareWave(int harmonicCount)
+    {
+        harmonics.clear();
+
+        for(int n = 0; n < harmonicCount; n++)
+        {
+            var coefficient = 2 * n + 1;
+            var amplitude = Constants.AMPLITUDE_SCALE / (coefficient * Math.PI);
+            generateHarmonicComponent(n, amplitude, coefficient, harmonicCount);
+        }
+        return harmonics;
+    }
+
+    public List<HarmonicVector> getSawToothWave(int harmonicCount)
+    {
+        harmonics.clear();
+
+        for(int n = 0; n < harmonicCount; n++)
+        {
+            var coefficient = n + 1;
+            var amplitude = Constants.AMPLITUDE_SCALE / (coefficient * Math.PI);
+            generateHarmonicComponent(n, amplitude, coefficient, harmonicCount);
+        }
+        return harmonics;
+    }
+
+    private void generateHarmonicComponent(int n, double amplitude, int coefficient, int harmonicCount)
+    {
+        HarmonicVector harmonic;
+
+        if(n == 0)
+        {
+            harmonic = new HarmonicVector(new Pair(Constants.LEFT_TRANSLATION, Constants.UP_TRANSLATION),
+                    new Pair(Constants.LEFT_TRANSLATION + amplitude * 2, Constants.UP_TRANSLATION + amplitude), amplitude, coefficient);
+        }
+        else
+        {
+            harmonic = new HarmonicVector(new Pair(harmonics.get(n - 1).getTerminalPoint().x - amplitude, harmonics.get(n - 1).getTerminalPoint().y - amplitude),
+                    new Pair(harmonics.get(n - 1).getTerminalPoint().x + amplitude, harmonics.get(n - 1).getTerminalPoint().y),
+                    amplitude, coefficient);
+        }
+
+        if(n == harmonicCount - 1)
+        {
+            harmonic.setIsLastHarmonic(true);
+        }
+
+        this.harmonics.add(harmonic);
+    }
+}
