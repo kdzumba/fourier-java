9fc97bc741be7d07a98f6f7749d3ed8747724a45
kdzumba <knowledgedzumba@gmail.com>
2022-06-27 09:36:24 +0200

    remove magic numbers

--- src/FourierSeries.java
+++ src/FourierSeries.java
@@ -12,8 +12,13 @@ public class FourierSeries extends JFrame implements ActionListener
     private final int LEFT = 50;
     private final int UP = 200;
     private final int DIAMETER = 150;
+    private final int WINDOW_WIDTH = 800;
+    private final int WINDOW_HEIGHT = 600;
+    private final int CURSOR_X_TRANSLATION = 400;
+    private final int TIMER_DELAY = 10;
+    private final int RADIUS = 75;
     private float angle = 0f;
-    private final double harmonicFrequency = 1d;
+    private final double HARMONIC_FREQUENCY = 0.1;
     private final List<HarmonicVector> harmonics;
     private final List<Double> waveHeights;
     public FourierSeries(String title)
@@ -21,15 +26,12 @@ public class FourierSeries extends JFrame implements ActionListener
         this.waveHeights = new ArrayList<>();
         this.harmonics = new ArrayList<>();
 
-        var firstHarmonic = new HarmonicVector(new Pair(LEFT + DIAMETER /2d, UP + DIAMETER / 2d), new Pair(LEFT + DIAMETER, UP + DIAMETER / 2d));
+        var firstHarmonic = new HarmonicVector(new Pair(LEFT + RADIUS, UP + RADIUS), new Pair(LEFT + RADIUS * 2, UP + RADIUS));
         firstHarmonic.setIsLastHarmonic(true);
         this.harmonics.add(firstHarmonic);
 
-        Timer timer = new Timer(10, this);
+        Timer timer = new Timer(TIMER_DELAY, this);
         timer.start();
-
-        int WINDOW_WIDTH = 800;
-        int WINDOW_HEIGHT = 600;
 
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setTitle(title);
@@ -49,7 +51,7 @@ public class FourierSeries extends JFrame implements ActionListener
         //Use traditional for loop here to avoid ConcurrentModificatoinException
         for(int index = 0; index < waveHeights.size(); index++)
         {
-            if(index + 400 > 800)
+            if(index + CURSOR_X_TRANSLATION > WINDOW_WIDTH)
             {
                 waveHeights.remove(waveHeights.get(index));
             }
@@ -63,20 +65,20 @@ public class FourierSeries extends JFrame implements ActionListener
         g.setColor(Color.WHITE);
 
         harmonics.forEach(harmonic -> {
-            harmonic.setTerminalPoint(new Pair(harmonic.getInitialPoint().x + DIAMETER / 2d + 75 * Math.cos(angle),
-                    harmonic.getInitialPoint().y + DIAMETER / 2d + 75 * Math.sin(angle)));
+            harmonic.setTerminalPoint(new Pair(harmonic.getInitialPoint().x + RADIUS + RADIUS * Math.cos(angle),
+                    harmonic.getInitialPoint().y + RADIUS + RADIUS * Math.sin(angle)));
             harmonic.paintComponent(g2d);
 
             if(harmonic.getIsLastHarmonic())
             {
-                drawCursor(g2d, harmonic.getTerminalPoint(), new Pair(400, harmonic.getTerminalPoint().y));
+                drawCursor(g2d, harmonic.getTerminalPoint(), new Pair(CURSOR_X_TRANSLATION, harmonic.getTerminalPoint().y));
                 this.waveHeights.add(0, harmonic.getTerminalPoint().y);
             }
         });
 
         this.removeOffScreenPoints();
         waveHeights.forEach(height -> {
-            g2d.draw(new Line2D.Double(400 + waveHeights.indexOf(height), height, 400 + waveHeights.indexOf(height), height));
+            g2d.draw(new Line2D.Double(CURSOR_X_TRANSLATION + waveHeights.indexOf(height), height, CURSOR_X_TRANSLATION + waveHeights.indexOf(height), height));
         });
     }
 
@@ -86,7 +88,7 @@ public class FourierSeries extends JFrame implements ActionListener
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        angle += 0.1;
+        angle += HARMONIC_FREQUENCY;
         this.repaint();
     }
 }
